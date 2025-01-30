b1dedd58bb6040c52db781172dc5da4d9bca6baa
René Crans <rene@gmail.com>
2025-01-30 12:57:20 +0100

    Reverted if and elif and added | to seqClass.py

--- seqClass.py
+++ seqClass.py
@@ -13,12 +13,14 @@ if len(sys.argv) == 1:
 
 args = parser.parse_args()
 
+
+# The code checks first if there is a U in the sequence and then for A,T,G or C --> Simply reverted the if and elif and added |
 args.seq = args.seq.upper() # Make all your letters in upper cases
 if re.search('^[ACGTU]+$', args.seq):
-    if re.search('T', args.seq): # Check if there is a T in your sequence
-        print ('The sequence is DNA')
-    elif re.search('U', args.seq):# Check if there is a U in your sequence
+    if re.search('U', args.seq):# Check if there is a U in your sequence
         print ('The sequence is RNA')
+    elif re.search('A|T|G|C', args.seq): # Check if there is a A, T, G or C in your sequence
+        print ('The sequence is DNA')
     else:
         print ('The sequence can be DNA or RNA')
 else:
