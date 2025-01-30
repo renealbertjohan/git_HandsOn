43ff83af0c01730398f4cc49dc4eb535b144c709
René Crans <rene@gmail.com>
2025-01-30 12:28:46 +0100

    Added some comments to the python script

--- seqClass.py
+++ seqClass.py
@@ -4,8 +4,8 @@ import sys, re
 from argparse import ArgumentParser
 
 parser = ArgumentParser(description = 'Classify a sequence as DNA or RNA')
-parser.add_argument("-s", "--seq", type = str, required = True, help = "Input sequence")
-parser.add_argument("-m", "--motif", type = str, required = False, help = "Motif")
+parser.add_argument("-s", "--seq", type = str, required = True, help = "Input sequence") # Indicate your sequence
+parser.add_argument("-m", "--motif", type = str, required = False, help = "Motif") # Indicate your motif you want to find in your sequence
 
 if len(sys.argv) == 1:
     parser.print_help()
@@ -13,11 +13,11 @@ if len(sys.argv) == 1:
 
 args = parser.parse_args()
 
-args.seq = args.seq.upper()
+args.seq = args.seq.upper() # Make all your letters in upper cases
 if re.search('^[ACGTU]+$', args.seq):
-    if re.search('T', args.seq):
+    if re.search('T', args.seq): # Check if there is a T in your sequence
         print ('The sequence is DNA')
-    elif re.search('U', args.seq):
+    elif re.search('U', args.seq):# Check if there is a U in your sequence
         print ('The sequence is RNA')
     else:
         print ('The sequence can be DNA or RNA')
@@ -25,9 +25,9 @@ else:
     print ('The sequence is not DNA nor RNA')
 
 if args.motif:
-    args.motif = args.motif.upper()
+    args.motif = args.motif.upper() # Make all your letter in upper case
     print(f'Motif search enabled: looking for motif "{args.motif}" in sequence "{args.seq}"... ', end = '')
     if re.search(args.motif, args.seq):
-        print("YES IT HAS BEEN FOUND")
+        print("YES")
     else:
         print("NO")
